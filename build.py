ba849cd4830fe640c9c842aef848d3ac327bad64
Opliko <opliko.reg@protonmail.com>
2023-10-22 23:46:01 +0200

    feat: minify production build

--- build.py
+++ build.py
@@ -4,28 +4,32 @@ import stickytape
 import sys
 import base64
 
-def build():
+import typer
+from typing import Annotated
+
+def build(minify: Annotated[bool, typer.Option("--minify", "-m", help="Minify the output (warning: will take a lot longer to build)")]=False):
     pathlib.Path("dist").mkdir(exist_ok=True)
     output = stickytape.script(
         "mn2/cli.py",
         python_binary=sys.executable,
         add_python_modules=["pygments", "pygments.lexers.python", "rich.ansi", "click._textwrap"],
-        minify=False
+        minify=minify
     )
-    output = python_minifier.minify(output,
-                                    filename='cli.py',
-                                    remove_literal_statements=False,
-                                    remove_annotations=False,
-                                    remove_pass=True,
-                                    combine_imports=True,
-                                    hoist_literals=False,
-                                    rename_globals=False,
-                                    rename_locals=False,
-                                    remove_asserts=True,
-                                    remove_debug=True,
-                                    remove_explicit_return_none=True,
-                                    preserve_shebang=False,
-                                    )
+    if minify:
+        output = python_minifier.minify(output,
+                                        filename='cli.py',
+                                        remove_literal_statements=False,
+                                        remove_annotations=False,
+                                        remove_pass=True,
+                                        combine_imports=True,
+                                        hoist_literals=False,
+                                        rename_globals=False,
+                                        rename_locals=False,
+                                        remove_asserts=True,
+                                        remove_debug=True,
+                                        remove_explicit_return_none=True,
+                                        preserve_shebang=False,
+                                        )
     
     encoded = base64.b85encode(output.encode('utf-8'))
 
@@ -35,4 +39,4 @@ def build():
     with open("dist/mn2.py", "w", encoding="utf-8") as f:
         f.write(output)
 if __name__ == "__main__":
-    build()
+    typer.run(build)
